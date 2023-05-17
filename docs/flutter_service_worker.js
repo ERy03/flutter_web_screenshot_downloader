7ec423410ce9cdf6bf05eb26c36a2f256917f771
Ryota <ryoenoki2013@gmail.com>
2023-05-17 13:40:38 +0900

    update: web build

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,9 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "e8ca1ed7bf47228df8ba9702b6ee2980",
-"index.html": "9f4d6206fc2a84eb378b812ded673838",
-"/": "9f4d6206fc2a84eb378b812ded673838",
+"index.html": "ab23591cbdef768eec1543be90e5773d",
+"/": "ab23591cbdef768eec1543be90e5773d",
 "main.dart.js": "0e7d712298f88a9da4b45a84ff9731ce",
+"flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -18,7 +19,11 @@ const RESOURCES = {
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
-"assets/assets/images/james-barker-v3-zcCWMjgM-unsplash.jpg": "fc59760bebdb1948993b5766dbe1ab20"
+"assets/assets/images/james-barker-v3-zcCWMjgM-unsplash.jpg": "fc59760bebdb1948993b5766dbe1ab20",
+"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
+"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
+"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
+"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e"
 };
 
 // The application shell files that are downloaded before a service worker can
