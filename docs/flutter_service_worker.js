f59146814beb64feee05d6217105d6f875eeef28
Ryota <ryoenoki2013@gmail.com>
2023-05-25 17:31:01 +0900

    update: build web with canvaskit

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "e8ca1ed7bf47228df8ba9702b6ee2980",
-"index.html": "736505cbce42b48c14720457412bc510",
-"/": "736505cbce42b48c14720457412bc510",
-"main.dart.js": "602a1d638cc78b7349c89f549e0a7a89",
+"index.html": "44608554bfd2537a7263bb5450afdde8",
+"/": "44608554bfd2537a7263bb5450afdde8",
+"main.dart.js": "1e67e8195363622db133553b8be14587",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
